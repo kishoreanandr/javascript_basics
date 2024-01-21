d0b41505a158d38ba4b1cf286c7ea7acabd2861e
kishoreanand.r <kishoreanand.r@gmail.com>
2024-01-21 23:16:02 +0530

    javascrpt basics added

---
+++ operators.js
@@ -0,0 +1,35 @@
+//Assignment Operator
+var a=100;
+console.log("Assignment Operator:",a)
+
+//Arithmetic operator
+var aA=20
+var bB=10
+console.log("Arithmetic Operator for addition:",aA+bB)
+console.log("Arithmetic Operator for subtraction:",aA-bB)
+console.log("Arithmetic Operator for multiplication:",aA*bB)
+console.log("Arithmetic Operator for quiotent:",aA/bB)
+console.log("Arithmetic Operator for remainder:",aA%bB)
+
+//Logical Operator -> &&,||,!
+var aa=10
+var bb="KISHORE"
+console.log("Logical Operator: ",aa&&bb)
+console.log("Logical Operator: ",aa||bb)
+console.log("Logical Operator: ",!aa)
+
+//&& Operator
+// console.log(10&&20)
+// console.log(10&&30)
+// console.log(10&&0)
+// console.log(0&&10)
+
+// or operator
+// console.log(10||20)
+// console.log(0||20)
+// console.log(20||0)
+// console.log(100||20&&0)
+console.log((100||20)&&0)
+console.log((100||0)&&20)
+console.log((100&&0)||20)
+console.log((100&&100)&&20)
---
+++ variable.js
@@ -0,0 +1,9 @@
+console.log("Hai" )
+a='a'
+console.log('Generable variable declration: '+a)
+var b='variable'
+console.log("This variable belongs to",b,"type.")
+let c='let'
+console.log("This variable belongs to",c,"type.")
+const d=10
+console.log("This variable",d,"belongs to conatant type")
